daa867e84ad6326186567a892a242fd0f8eb8c1c
alexh01 <aheifler@stevens.edu>
2022-12-06 19:29:50 -0500

    created classifier file will add in rest wed

---
+++ classifier.py
@@ -0,0 +1,5 @@
+# links for later
+
+# https://towardsdatascience.com/efficient-k-means-clustering-algorithm-with-optimum-iteration-and-execution-time-9358a794406c#603e
+# https://github.com/ahegel/genre-ML-clustering/blob/master/genre_clustering.py
+# https://www.youtube.com/watch?v=AQ4zCWn7y-0
